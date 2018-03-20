a2c5aa6575d10f8ec25ec8adcc70e36afc1d7229
Jack Evans <jack@evans.gb.net>
2018-03-20 08:00:03 +0000

    Added checkbox toggling and image modal view

--- src/Update.elm
+++ src/Update.elm
@@ -10,6 +10,7 @@ import Dom
 import Dom.Scroll
 import List.Extra exposing (elemIndex, getAt)
 import Task
+import Set as S
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -34,7 +35,7 @@ update msg model =
                                     , previousSearch = query
                                     , viewing = Nothing
                                   }
-                                , searchImage query
+                                , searchImage query model.sources
                                 )
 
                             Nothing ->
@@ -65,28 +66,32 @@ update msg model =
                                     in
                                         case currentIndex of
                                             Just index ->
-                                                let
-                                                    newIndex =
-                                                        case code of
-                                                            37 ->
-                                                                index - 1
-
-                                                            39 ->
-                                                                index + 1
-
-                                                            _ ->
-                                                                index
-                                                in
+                                                if code == 27 then
+                                                    -- Escape Key
+                                                    ( { model | viewing = Nothing }, Cmd.none )
+                                                else
                                                     let
-                                                        nextPhoto =
-                                                            getAt newIndex photos
-                                                    in
-                                                        case nextPhoto of
-                                                            Just photo ->
-                                                                ( { model | viewing = Just photo }, Cmd.none )
+                                                        newIndex =
+                                                            case code of
+                                                                37 ->
+                                                                    index - 1
 
-                                                            Nothing ->
-                                                                ( model, Cmd.none )
+                                                                39 ->
+                                                                    index + 1
+
+                                                                _ ->
+                                                                    index
+                                                    in
+                                                        let
+                                                            nextPhoto =
+                                                                getAt newIndex photos
+                                                        in
+                                                            case nextPhoto of
+                                                                Just photo ->
+                                                                    ( { model | viewing = Just photo }, Cmd.none )
+
+                                                                Nothing ->
+                                                                    ( model, Cmd.none )
 
                                             _ ->
                                                 ( model, Cmd.none )
@@ -103,12 +108,22 @@ update msg model =
         Msgs.FetchPhotos response ->
             ( { model | photos = response }, Cmd.none )
 
+        Msgs.ToggleCheckbox target ->
+            let
+                newSources =
+                    if S.member target model.sources then
+                        S.remove target model.sources
+                    else
+                        S.insert target model.sources
+            in
+                ( { model | sources = newSources }, Cmd.none )
+
         Msgs.SearchSubmit ->
             -- Dont do anything if the query is empty
             if String.isEmpty model.query then
                 ( model, Cmd.none )
             else
-                ( model, Cmd.batch [ newUrl ("/?q=" ++ model.query), searchImage model.query ] )
+                ( model, Cmd.batch [ newUrl ("/?q=" ++ model.query), searchImage model.query model.sources ] )
 
         Msgs.ClearInput ->
             ( { model | query = "" }, Task.attempt (always Msgs.NoOp) <| Dom.focus "searchInput" )
